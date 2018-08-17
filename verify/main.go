1f632a6aa119eea6c166628aabdea0a5bd8dd1f9
Ashok Modi <btmash@gmail.com>
2018-08-16 19:05:48 -0700

    Updates with what was done after testing

--- verify/main.go
+++ verify/main.go
@@ -1,51 +1,86 @@
 package main
 
 import (
-  "fmt"
-  "encoding/json"
-  "github.com/smancke/mailck"
-  "github.com/aws/aws-lambda-go/events"
-  "github.com/aws/aws-lambda-go/lambda"
+	"fmt"
+	"encoding/json"
+	"time"
+
+	"github.com/smancke/mailck"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type emailStatus struct {
-  Email string `json:"email"`
-  Status string `json:"status"`
-  Message string `json:"message"`
+	Email            string `json:"email"`
+	ValidSyntax      bool   `json:"syntax_valid"`
+	DisposableStatus bool   `json:"is_disposable_address"`
+	SmtpStatus       string `json:"smtp_valid"`
+	Message          string `json:"message"`
 }
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-  result, _ := mailck.Check("btmash@gmail.com", request.Body)
-  returnData := new(emailStatus)
-  returnData.Email = "btmash@gmail.com"
-  switch {
+	ch := make(chan *emailStatus, 1)
+	returnData := new(emailStatus)
 
-    case result.IsValid():
-      returnData.Status = "valid"
-      returnData.Message = "Perfectly valid email"
+	// Create a goroutine that will attempt to connect to the SMTP server
+	go func() {
+		result, _ := mailck.Check("no-reply@card.com", request.Body)
+		returnData := new(emailStatus)
+		returnData.Email = request.Body
+		switch {
 
-    case result.IsError():
-      returnData.Status = "error"
-      returnData.Message = "SMTP Failed. May or may not be valid email"
+		case result.IsValid():
+			returnData.ValidSyntax = true
+			returnData.DisposableStatus = false
+			returnData.SmtpStatus = "true"
+			returnData.Message = "Perfectly valid email"
 
-    case result.IsInvalid():
-      returnData.Status = "invalid"
-      switch (result) {
-        case mailck.InvalidDomain:
-          returnData.Message = "Invalid Domain provided"
-        case mailck.InvalidSyntax:
-          returnData.Message = "Invalid email syntax"
-        case mailck.MailboxUnavailable:
-          returnData.Message = "Email address does not exist"
-        case mailck.Disposable:
-          returnData.Message = "Disposable email address. You may choose to test further and see if this is fine"
-      }
-  }
-  jsonResult, _ := json.Marshal(returnData)
-  fmt.Println(string(jsonResult))
-  return events.APIGatewayProxyResponse{Body: string(jsonResult), StatusCode: 200}, nil
+		case result.IsError():
+			returnData.ValidSyntax = true
+			returnData.DisposableStatus = false
+			returnData.SmtpStatus = "error"
+			returnData.Message = "SMTP Failed. May or may not be valid email"
+
+		case result.IsInvalid():
+			returnData.SmtpStatus = "false"
+			switch result {
+			case mailck.InvalidDomain:
+				returnData.ValidSyntax = false
+				returnData.DisposableStatus = false
+				returnData.Message = "Invalid Domain provided"
+			case mailck.InvalidSyntax:
+				returnData.ValidSyntax = false
+				returnData.DisposableStatus = false
+				returnData.Message = "Invalid email syntax"
+			case mailck.MailboxUnavailable:
+				returnData.ValidSyntax = true
+				returnData.DisposableStatus = false
+				returnData.Message = "Email address does not exist"
+			case mailck.Disposable:
+				returnData.ValidSyntax = true
+				returnData.DisposableStatus = true
+				returnData.Message = "Disposable email address."
+			}
+		}
+		ch <- returnData
+	}()
+
+	select {
+	case validate := <-ch:
+		returnData = validate
+	case <-time.After(10 * time.Second):
+		returnData.Email = request.Body
+		returnData.ValidSyntax = true
+		returnData.DisposableStatus = true
+		returnData.SmtpStatus = "error"
+		returnData.Message = "SMTP Dial timed out. May or may not be valid email"
+	}
+
+	jsonResult, _ := json.Marshal(returnData)
+	fmt.Println(string(jsonResult))
+	return events.APIGatewayProxyResponse{Body: string(jsonResult), StatusCode: 200}, nil
 }
 
 func main() {
-  lambda.Start(Handler)
+	lambda.Start(Handler)
 }
